64bb28734221d0360f40c4649c96226a8f6eff23
Jonas Rockstroh <jo@w1b.de>
2009-11-22 19:50:25 +0100

    premerge same version

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #define P_SCL PC4
 #define P_RCK PC2
 
+<<<<<<< HEAD
 unsigned int PWM[12];
 int X, dX, eX, XChanged;
 int YStart[], YEnd[], Y[], dY[], eY[];
@@ -36,12 +37,51 @@ struct pattern AnimationA[]={
 	0, 255, 0,
 	0, 0, 255,
 	80, 80, 80}, 10, 0},
+=======
+#define MaxLEDs 64*3
+#define PWMres 100
+ 
+uint8_t PWM[MaxLEDs];
+int8_t X, dX, eX, XChanged, YStart, YEnd;
+int8_t dY[MaxLEDs], eY[MaxLEDs];
+
+uint8_t fademode = 0;
+const uint8_t numanimas = 2;
+uint8_t mux, idx;
+ 
+struct pattern {
+	uint8_t hold;
+	uint8_t fade;
+	uint8_t pwm[MaxLEDs];
+};
+
+
+const struct pattern AnimationA[2] ={
+	{
+	10,1,
+	{
+	255, 255, 255,
+	255, 255, 255,
+	255, 255, 255,
+	255, 255, 255}},
+
+  	{
+	20,1,
+  	{
+	255, 0, 0,
+	0, 255, 0,
+	0, 0, 255,
+	80, 80, 80}},
+>>>>>>> u60/master
 
 
 };
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> u60/master
 void setLED(int state) 
 {
 	if (state == 1 ) PORTC &= ~(1 << P_IN); else PORTC |= (1 << P_IN);
@@ -60,6 +100,7 @@ void commit()
 
 void fade() //2 bresenhams
 {
+<<<<<<< HEAD
 	unsigned int i;
 
 	while (XChanged != 1)
@@ -69,45 +110,87 @@ void fade() //2 bresenhams
 			if (dY[i] > 0) 
 			{
 				if (2*(eY[i] + dY[i]) < 255)
+=======
+	int i;
+
+	while (XChanged != 1)
+	{
+	    if (fademode == 1) XChanged = 1; 
+		for (i = 0; i < MaxLEDs; i++)
+		{
+			if (dY[i] > 0) 
+			{
+				if (2*(eY[i] + dY[i]) < PWMres)
+>>>>>>> u60/master
 				{
 					eY[i] += dY[i];
 				}
 				else
 				{
+<<<<<<< HEAD
 					Y[i]++;
 					eY[i] += (dY[i]-255);
+=======
+					PWM[i]++;
+					eY[i] += (dY[i]-PWMres);
+>>>>>>> u60/master
 				}
 			}
 			else
 			{
+<<<<<<< HEAD
 				if (2*(eY[i] + dY[i]) > -255)
+=======
+				if (2*(eY[i] + dY[i]) > -PWMres)
+>>>>>>> u60/master
 				{
 					eY[i] += dY[i];
 				}
 				else
 				{
+<<<<<<< HEAD
 					Y[i]++;
 					eY[i] += (dY[i]+255);
 				}
 			}
 		
 		if (2*(eX + dX) < 255)
+=======
+					PWM[i]++;
+					eY[i] += (dY[i]+PWMres);
+				}
+			}
+		}
+		
+		if (2*(eX + dX) < PWMres)
+>>>>>>> u60/master
 		{
 			eX += dX;
 		}
 		else
 		{
 			X++;
+<<<<<<< HEAD
 			eX += dX-255;
+=======
+			eX += dX-PWMres;
+>>>>>>> u60/master
 			//setled mist
 			XChanged = 1;
 		}
 
+<<<<<<< HEAD
 		}
 	}
 }
 
 void main()
+=======
+	}
+}
+
+int main()
+>>>>>>> u60/master
 {
 	unsigned int i, k, patterncntr, hold;
 
@@ -115,19 +198,27 @@ void main()
 	DDRC = (1 << DDC_IN) | (1 << DDC_SCK) | (1 << DDC_G) | (1 << DDC_SCL) | (1 << DDC_RCK);
 	
 	
+<<<<<<< HEAD
 
+=======
+>>>>>>> u60/master
 	PORTC = (1 << P_G) | (1 << P_SCL);
 	patterncntr = 0;
 
 	while (1) 
 	{
 	
+<<<<<<< HEAD
 		while (patterncntr <= 1)
+=======
+		while (patterncntr < numanimas)
+>>>>>>> u60/master
 		{
 			X = 0;
 			eX = 0;
 			XChanged = 0;
 
+<<<<<<< HEAD
 			for (i = 0; i<=11; i++)
 			{
 				eY[i] = 0;
@@ -138,13 +229,62 @@ void main()
 				dY[i] = YEnd[i]-YStart[i];
 				dX = AnimationA[patterncntr].hold;
 
+=======
+			for (i = 0; i < MaxLEDs; i++)
+			{
+				eY[i] = 0;
+			  	YStart = AnimationA[patterncntr].pwm[i];
+				
+				if (patterncntr < numanimas-1) YEnd = AnimationA[patterncntr+1].pwm[i]; else YEnd = AnimationA[0].pwm[i];
+				//Y[i] = YStart;
+				dY[i] = YEnd-YStart;
+				dX = AnimationA[patterncntr].hold;
+>>>>>>> u60/master
 				PWM[i] = AnimationA[patterncntr].pwm[i];
 			}
 
 			while (hold <= AnimationA[patterncntr].hold)
 			{
+<<<<<<< HEAD
 				for (i=0; i<=255; i++)
 				{	
+=======
+				for (i=0; i<=PWMres; i++)
+				{	
+					fademode = AnimationA[patterncntr].fade;
+					if (fademode > 0) fade();	
+					for (mux=0; mux<2; mux++)
+					{
+						for (k=0; k<=5; k++)
+						{	
+							idx = k*mux*2;
+							if (i < PWM[idx]) setLED(1); else setLED(0);
+						}
+						switch (mux)
+						{
+							case 0 :
+							   setLED(1);
+							   setLED(0);
+							   break;
+							case 1 :
+							   setLED(0);
+							   setLED(1);
+							   break;
+							case 2 :
+							   setLED(1);
+							   setLED(0);
+							   break;
+							case 3 :
+							   setLED(1);
+							   setLED(0);
+							   break;
+						}	
+						commit();
+					}
+					PORTC &= ~(1 << P_G); // OE
+
+					/*
+>>>>>>> u60/master
 					for (k=0; k<=5; k++)
 					{	
 						if (i < PWM[k]) setLED(1); else setLED(0);
@@ -161,6 +301,10 @@ void main()
 					setLED(1);
 					commit();
 					PORTC &= ~(1 << P_G);
+<<<<<<< HEAD
+=======
+					*/
+>>>>>>> u60/master
 				}
 				hold++;	
 			} 
